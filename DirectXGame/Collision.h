b91fc44cadb18a56c381443009cc38290c47738f
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-07-11 16:27:54 +0900

    レイと球の当たり判定

--- DirectXGame/Collision.h
+++ DirectXGame/Collision.h
@@ -18,25 +18,6 @@ public:
 		DirectX::XMVECTOR* inter = nullptr);
 
 	/// <summary>
-	/// 点と三角形の最近接点を求める
-	/// </summary>
-	/// <param name="point">点</param>
-	/// <param name="triangle">三角形</param>
-	/// <param name="closest">最近接点(出力用)</param>
-	static void ClosestPtPoint2Triangle(const DirectX::XMVECTOR& point, const Triangle& triangle,
-		DirectX::XMVECTOR* closest);
-
-	/// <summary>
-	/// 球と法線付き三角形の当たりチェック
-	/// </summary>
-	/// <param name="sphere">球</param>
-	/// <param name="triangle">三角形</param>
-	/// <param name="inter">交点(平面上の最近接点)</param>
-	/// <returns>交差しているか否か</returns>
-	static bool CheckSphere2Triangle(const Sphere& sphere, const Triangle& triangle,
-		DirectX::XMVECTOR* inter = nullptr);
-
-	/// <summary>
 	/// レイと平面の当たり判定
 	/// </summary>
 	/// <param name="lay">レイ</param>
@@ -45,17 +26,6 @@ public:
 	/// <param name="inter">交点(平面上の最近接点)</param>
 	/// <returns>交差しているか否か</returns>
 	static bool CheckRay2Plane(const Ray& ray, const Plane& plane,
-		float* distance = nullptr, DirectX::XMVECTOR* inter = nullptr);
-
-	/// <summary>
-	/// レイと法線付き三角形の当たり判定
-	/// </summary>
-	/// <param name="lay">レイ</param>
-	/// <param name="triangle">三角形</param>
-	/// <param name="inter">距離</param>
-	/// <param name="inter">交点(平面上の最近接点)</param>
-	/// <returns>交差しているか否か</returns>
-	static bool CheckRay2Triangle(const Ray& ray, const Triangle& triangle,
 		float* distance = nullptr, DirectX::XMVECTOR* inter = nullptr);
 
 	/// <summary>
