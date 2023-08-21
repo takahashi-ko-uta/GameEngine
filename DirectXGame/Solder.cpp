d2c51e8ba83998d1f9f9beb3f9693a227b8471f4
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-08-21 23:48:22 +0900

    仮で兵を増やした

--- DirectXGame/Solder.cpp
+++ DirectXGame/Solder.cpp
@@ -1,5 +1,75 @@
 #include "Solder.h"
 
+#pragma region Normal
+void Normal::Initialize(int soldierNum,int normalNum)
+{
+    this->normalNum_ = normalNum;
+    //モデル読み込み
+    model_ = Model::LoadFromOBJ("Soldier");
+    //球のモデル初期化
+    obj_ = Object3d::Create();
+    obj_->SetModel(model_);
+    obj_->SetScale({ 0.5f,0.5f,0.5f });
+    if (soldierNum == 0) { obj_->SetColor({ 1,0,0,1 }); }
+    if (soldierNum == 1) { obj_->SetColor({ 0,1,0,1 }); }
+    if (soldierNum == 2) { obj_->SetColor({ 0,0,1,1 }); }
+    if (soldierNum == 3) { obj_->SetColor({ 1,0,1,1 }); }
+}
+void Normal::Finalize()
+{
+
+}
+void Normal::Update(XMFLOAT3 pos)
+{
+    //リーダーの位置保存
+    leaderPos = pos;
+    XMFLOAT3 objPos = obj_->GetPosition();
+
+    switch (normalNum_)
+    {
+    case 0://上
+        goalPos = { leaderPos.x,leaderPos.y,leaderPos.z + 3.0f };
+        break;
+
+    case 1://右上
+        goalPos = { leaderPos.x + 3.0f,leaderPos.y,leaderPos.z + 3.0f };
+        break;
+
+    case 2://右
+        goalPos = { leaderPos.x + 3.0f,leaderPos.y,leaderPos.z };
+        break;
+
+    case 3://右下
+        goalPos = { leaderPos.x + 3.0f,leaderPos.y,leaderPos.z - 3.0f };
+        break;
+
+    case 4://下
+        goalPos = { leaderPos.x,leaderPos.y,leaderPos.z - 3.0f };
+        break;
+
+    case 5://左下
+        goalPos = { leaderPos.x - 3.0f,leaderPos.y,leaderPos.z - 3.0f };
+        break;
+
+    case 6://左
+        goalPos = { leaderPos.x - 3.0f,leaderPos.y,leaderPos.z };
+        break;
+
+    case 7://左上
+        goalPos = { leaderPos.x - 3.0f,leaderPos.y,leaderPos.z + 3.0f };
+        break;
+
+    }
+
+    obj_->SetPosition(goalPos);
+    obj_->Update();
+}
+void Normal::Draw()
+{
+    obj_->Draw();
+}
+#pragma endregion
+
 #pragma region Leader
 void Leader::Initialize(int soldierNum)
 {
@@ -8,7 +78,7 @@ void Leader::Initialize(int soldierNum)
     //球のモデル初期化
     obj_ = Object3d::Create();
     obj_->SetModel(model_);
-    obj_->SetScale({ 1.0f,1.0f,1.0f });
+    obj_->SetScale({ 0.5f,0.5f,0.5f });
     if (soldierNum == 0) { obj_->SetColor({ 1,0,0,1 }); }
     if (soldierNum == 1) { obj_->SetColor({ 0,1,0,1 }); }
     if (soldierNum == 2) { obj_->SetColor({ 0,0,1,1 }); }
@@ -45,10 +115,15 @@ void Solider::Initialize(XMFLOAT3 spawnPos, int soldierNum)
     soldierRoute_ = new SoldierRoute();
     soldierRoute_->Initialize(spawnPos, soldierNum);
 
-    //リーダーの初期化
+    //リーダー(真ん中のやつ)の初期化
     leader = new Leader();
     leader->Initialize(soldierNum);
-
+    //その他の兵隊の初期化
+    for (int i = 0; i < 8; i++) {
+        normal[i] = new Normal();
+        normal[i]->Initialize(soldierNum,i);
+    }
+    
 }
 
 void Solider::Finalize()
@@ -60,12 +135,19 @@ void Solider::Update(XMINT2 startFloor, XMINT2 goalFloor, XMFLOAT3 floorPos[11][
 {
     soldierRoute_->Update(startFloor, goalFloor, floorPos,selectSoldier);
     leader->Update(soldierRoute_->GetPosition());
+    for (int i = 0; i < 8; i++) {
+        normal[i]->Update(leader->GetPosition());
+    }
+    
 }
 
 void Solider::Draw()
 {
     soldierRoute_->Draw();
     leader->Draw();
+    for (int i = 0; i < 8; i++) {
+        normal[i]->Draw();
+    }
 }
 #pragma endregion
 
--- DirectXGame/Solder.h
+++ DirectXGame/Solder.h
@@ -3,7 +3,27 @@
 #include "Object3d.h"
 #include "SoldierRoute.h"
 
+class Normal
+{
+public:
+	//初期化
+	void Initialize(int soldierNum, int normalNum);
+	//終了
+	void Finalize();
+	//毎フレーム処理
+	void Update(XMFLOAT3 pos);
+	//描画
+	void Draw();
 
+private:
+	Model* model_ = nullptr;
+	Object3d* obj_ = nullptr;
+
+	int normalNum_;
+	XMFLOAT3 leaderPos;//指定されている床の座標
+	XMFLOAT3 goalPos;//目的地
+
+};
 
 
 class Leader
@@ -18,6 +38,8 @@ public:
 	//描画
 	void Draw();
 
+	//getter
+	XMFLOAT3 GetPosition() { return obj_->GetPosition(); }
 private:
 	Model* model_ = nullptr;
 	Object3d* obj_ = nullptr;
@@ -44,4 +66,5 @@ public:
 private:
 	SoldierRoute* soldierRoute_ = nullptr;
 	Leader* leader = nullptr;
+	Normal* normal[8];
 };
